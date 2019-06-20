a601b851c3b2dfd8ab3fd578a4a7c5fb6eb1ef4d
anup <np.ghsh@gmail.com>
2019-06-20 20:43:21 +1000

    Setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,57 +1,60 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: ""
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
   };
-
 
   firebase.initializeApp(config);
 
   const database = firebase.database();
-  database.ref().remove();
+
+export {firebase, database as default};
+
+
+  //database.ref().remove();
   // Setup "expenses" with three items (description, note, amount, createdAt)
 
-database.ref('expenses').push({
-    description: 'Rent',
-    note: '',
-    amount: 109500,
-    createdAt: 976123498763
-  });
+// database.ref('expenses').push({
+//     description: 'Rent',
+//     note: '',
+//     amount: 109500,
+//     createdAt: 976123498763
+//   });
   
-  database.ref('expenses').push({
-    description: 'Phone bill',
-    note: '',
-    amount: 5900,
-    createdAt: 976123498763
-  });
+//   database.ref('expenses').push({
+//     description: 'Phone bill',
+//     note: '',
+//     amount: 5900,
+//     createdAt: 976123498763
+//   });
   
-  database.ref('expenses').push({
-    description: 'Food',
-    note: '',
-    amount: 1200,
-    createdAt: 976123498763
-  });
+//   database.ref('expenses').push({
+//     description: 'Food',
+//     note: '',
+//     amount: 1200,
+//     createdAt: 976123498763
+//   });
   
   
-  // child_removed
-  database.ref('expenses').on('child_removed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  });
+//   // child_removed
+//   database.ref('expenses').on('child_removed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   });
   
-  // child_changed
-  database.ref('expenses').on('child_changed', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  });
+//   // child_changed
+//   database.ref('expenses').on('child_changed', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   });
   
-  // child_added
-  database.ref('expenses').on('child_added', (snapshot) => {
-    console.log(snapshot.key, snapshot.val());
-  });
+//   // child_added
+//   database.ref('expenses').on('child_added', (snapshot) => {
+//     console.log(snapshot.key, snapshot.val());
+//   });
   
   // database.ref('expenses')
   //   .once('value')
--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,5 +1,10 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import { startAddExpense,addExpense, editExpense, removeExpense } from '../../actions/expenses';
+import expenses from '../fixtures/expenses';
+import database from '../../firebase/firebase'
 
+const createMockStore = configureMockStore([thunk]);
 test('should setup remove expense',() => {
     const action = removeExpense({id:'123abc'});
     expect(action).toEqual({
@@ -18,32 +23,76 @@ test('should setup edit expense',() => {
 });
 
 test('test add expense with provided value',() => {
-    const expenseData ={
-        description: 'Rent',
-        amount: 100,
-        createdAt: 1000,
-        note:'This was the last month rent'
-    };
-    const action = addExpense(expenseData);
+ 
+    const action = addExpense(expenses[2]);
     expect(action).toEqual({
         type:'ADD_EXPENSE',
-        expense:{
-            ...expenseData,
-            id: expect.any(String)
-        }
+        expense:expenses[2]
     });
 });
 
-test('test add expense with default value',() => {   
-    const action = addExpense();
-    expect(action).toEqual({
-        type:'ADD_EXPENSE',
-        expense:{
-            description:'',
-            note:'',
-            amount :0,
-            createdAt:0,
-            id: expect.any(String)
-        }
+
+test('test add expense to database and store',(done) => {   
+    const store = createMockStore ({});
+    const expenseData ={
+        description:'mouse',
+        amount :3000,
+        note:'this is test data',
+        createdAt:1000
+    };
+    store.dispatch(startAddExpense(expenseData)).then(()=>{
+       const actions = store.getActions();
+       expect(actions[0]).toEqual({
+           type:'ADD_EXPENSE',
+           expense:{
+            id:expect.any(String),
+            ...expenseData
+           }
+       });
+
+       return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+        }).then((snapshot)=>{
+            expect(snapshot.val()).toEqual(expenseData);
+        done();
     });
 });
+
+
+test('test add expense with defaults to database and store',(done) => {   
+    const store = createMockStore ({});
+    const expenseDefaults = {
+        description : '',
+        note : '',
+        amount : 0,
+        createdAt : 0
+    };
+    store.dispatch(startAddExpense({})).then(()=>{
+       const actions = store.getActions();
+       expect(actions[0]).toEqual({
+           type:'ADD_EXPENSE',
+           expense:{
+            id:expect.any(String),
+            ...expenseDefaults
+           }
+       });
+
+       return database.ref(`expenses/${actions[0].expense.id}`).once('value');
+        }).then((snapshot)=>{
+            expect(snapshot.val()).toEqual(expenseDefaults);
+        done();
+    });
+    });
+
+// test('test add expense with default value',() => {   
+//     const action = addExpense();
+//     expect(action).toEqual({
+//         type:'ADD_EXPENSE',
+//         expense:{
+//             description:'',
+//             note:'',
+//             amount :0,
+//             createdAt:0,
+//             id: expect.any(String)
+//         }
+//     });
+// });
